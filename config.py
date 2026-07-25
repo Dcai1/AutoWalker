614abaf6b5171da19dc28f3e09d01858e2960880
David C <dcai1.3828@gmail.com>
2026-07-25 12:59:27 -0400

    - Attached configuration file for DEFAULT key bindings and window settings

---
+++ config.py
@@ -0,0 +1,10 @@
+from pynput.keyboard import Key, KeyCode
+
+# Default Hold key: w
+# Default Toggle key: f6
+
+HOLD_KEY = KeyCode.from_char('w')
+TOGGLE_KEY = Key.f6
+WINDOW_TITLE = "Auto Holder v1.0.1"
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 300
