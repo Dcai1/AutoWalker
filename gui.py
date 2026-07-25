dbe6eae54582f6fd2ccbec52949f3d45ec814d9a
David C <dcai1.3828@gmail.com>
2026-07-25 13:00:12 -0400

    - Implemented main application window and connected with functions for behavior and key binding functionality

---
+++ gui.py
@@ -0,0 +1,166 @@
+from PySide6.QtWidgets import (
+    QWidget, QVBoxLayout, QLabel, QPushButton
+)
+from PySide6.QtCore import Qt, QTimer
+from pynput.keyboard import Key, KeyCode
+
+from config import WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT
+from icon import create_app_icon
+
+
+def key_to_str(key):
+    if isinstance(key, KeyCode):
+        if key.char is not None:
+            return key.char.upper()
+        return str(key)
+    elif isinstance(key, Key):
+        name = str(key).replace('Key.', '').upper()
+        return name
+    return str(key).upper()
+
+
+class AutoHolderWindow(QWidget):
+    def __init__(self, holder):
+        super().__init__()
+        self._holder = holder
+        self._capture_target = None
+
+        self.setWindowTitle(WINDOW_TITLE)
+        # change placeholder when actual icon is acquired
+        self.setWindowIcon(create_app_icon())
+        
+        self.setStyleSheet("background-color: #f0f0f0;")
+
+        self._setup_ui()
+        self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+        self._connect_signals()
+        self._update_display()
+
+    def _setup_ui(self):
+        layout = QVBoxLayout()
+        layout.setContentsMargins(15, 12, 15, 12)
+        layout.setSpacing(6)
+
+        hold_label = QLabel("Key to Hold:")
+        self.hold_display = QPushButton()
+        self.hold_display.setFixedHeight(32)
+        self.hold_display.setCursor(Qt.PointingHandCursor)
+        self.hold_display.clicked.connect(self._start_hold_capture)
+
+        hold_hint = QLabel("Click above and press any key to rebind")
+        hold_hint.setStyleSheet("color: gray; font-size: 11px;")
+
+        layout.addWidget(hold_label)
+        layout.addWidget(self.hold_display)
+        layout.addWidget(hold_hint)
+
+        layout.addSpacing(6)
+
+        toggle_label = QLabel("Toggle Keybind:")
+        self.toggle_display = QPushButton()
+        self.toggle_display.setFixedHeight(32)
+        self.toggle_display.setCursor(Qt.PointingHandCursor)
+        self.toggle_display.clicked.connect(self._start_toggle_capture)
+
+        toggle_hint = QLabel("Click above and press any key to rebind")
+        toggle_hint.setStyleSheet("color: gray; font-size: 11px;")
+
+        layout.addWidget(toggle_label)
+        layout.addWidget(self.toggle_display)
+        layout.addWidget(toggle_hint)
+
+        layout.addSpacing(10)
+
+        self.start_stop_btn = QPushButton()
+        self.start_stop_btn.setFixedHeight(36)
+
+        layout.addWidget(self.start_stop_btn)
+
+        layout.addStretch()
+
+        self.msg_label = QLabel()
+        self.msg_label.setAlignment(Qt.AlignCenter)
+        self.msg_label.setWordWrap(True)
+        self.msg_label.setStyleSheet("color: #cc0000; font-size: 11px;")
+
+        self.status_label = QLabel()
+        self.status_label.setStyleSheet("color: gray; font-size: 11px;")
+        self.status_label.setAlignment(Qt.AlignCenter)
+
+        admin_hint = QLabel(
+            "If the keys aren't working in-game, try running this program as an Administrator."
+        )
+        admin_hint.setStyleSheet("color: gray; font-size: 11px;")
+        admin_hint.setAlignment(Qt.AlignCenter)
+        admin_hint.setWordWrap(True)
+
+        layout.addWidget(self.msg_label)
+        layout.addWidget(self.status_label)
+        layout.addWidget(admin_hint)
+
+        self.setLayout(layout)
+
+    def _connect_signals(self):
+        self._holder.running_changed.connect(self._on_running_changed)
+        self._holder.key_captured.connect(self._on_key_captured)
+        self.start_stop_btn.clicked.connect(self._toggle_start_stop)
+
+    def _on_running_changed(self, running):
+        self.start_stop_btn.setText("Stop" if running else "Start")
+
+    def _toggle_start_stop(self):
+        self._holder.toggle()
+
+    def _start_hold_capture(self):
+        self._capture_target = 'hold'
+        self.hold_display.setText("Press a key...")
+        self._holder.start_capture()
+
+    def _start_toggle_capture(self):
+        self._capture_target = 'toggle'
+        self.toggle_display.setText("Press a key...")
+        self._holder.start_capture()
+
+    def _on_key_captured(self, key):
+        if key is None:
+            self._capture_target = None
+            self._update_display()
+            return
+
+        if self._capture_target == 'hold':
+            success = self._holder.set_hold_key(key)
+            if success:
+                self.hold_display.setText(key_to_str(self._holder.hold_key))
+                self._show_message(f"Hold key changed to {key_to_str(key)}", "green")
+            else:
+                self.hold_display.setText(key_to_str(self._holder.hold_key))
+                self._show_message("Cannot use the same key as toggle key!", "red")
+        elif self._capture_target == 'toggle':
+            success = self._holder.set_toggle_key(key)
+            if success:
+                self.toggle_display.setText(key_to_str(self._holder.toggle_key))
+                self._show_message(f"Toggle key changed to {key_to_str(key)}", "green")
+            else:
+                self.toggle_display.setText(key_to_str(self._holder.toggle_key))
+                self._show_message("Cannot use the same key as hold key!", "red")
+        else:
+            self._update_display()
+        self._capture_target = None
+
+    def _show_message(self, text, color="green"):
+        self.msg_label.setStyleSheet(f"color: {color}; font-size: 11px;")
+        self.msg_label.setText(text)
+        QTimer.singleShot(3000, self._clear_message)
+
+    def _clear_message(self):
+        self.msg_label.clear()
+
+    def _update_display(self):
+        self.hold_display.setText(key_to_str(self._holder.hold_key))
+        self.toggle_display.setText(key_to_str(self._holder.toggle_key))
+        running = self._holder.is_running
+        self.start_stop_btn.setText("Stop" if running else "Start")
+
+    def closeEvent(self, event):
+        self._holder.stop()
+        event.accept()
---
+++ holder.py
@@ -0,0 +1,100 @@
+from threading import Lock
+from PySide6.QtCore import QObject, Signal
+from pynput.keyboard import Listener, Controller, Key, KeyCode
+
+from config import HOLD_KEY, TOGGLE_KEY
+
+
+class KeyHolder(QObject):
+    running_changed = Signal(bool)
+    key_captured = Signal(object)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self._running = False
+        self._hold_key = HOLD_KEY
+        self._toggle_key = TOGGLE_KEY
+        self._controller = Controller()
+        self._lock = Lock()
+        self._capturing = False
+
+        self._listener = Listener(on_press=self._on_press)
+        self._listener.start()
+
+    def _on_press(self, key):
+        captured_key = None
+        should_toggle = False
+        with self._lock:
+            if self._capturing:
+                self._capturing = False
+                if key != Key.esc:
+                    captured_key = key
+            elif key == self._toggle_key:
+                self._running = not self._running
+                if self._running:
+                    self._controller.press(self._hold_key)
+                else:
+                    self._controller.release(self._hold_key)
+                should_toggle = True
+        if captured_key is not None:
+            self.key_captured.emit(captured_key)
+        elif captured_key is None and should_toggle is False:
+            pass
+        if should_toggle:
+            self.running_changed.emit(self._running)
+
+    def start_capture(self):
+        with self._lock:
+            self._capturing = True
+
+    def set_hold_key(self, key):
+        with self._lock:
+            if key == self._toggle_key:
+                return False
+            was_running = self._running
+            if was_running:
+                self._controller.release(self._hold_key)
+            self._hold_key = key
+            if was_running:
+                self._controller.press(self._hold_key)
+            return True
+
+    def set_toggle_key(self, key):
+        with self._lock:
+            if key == self._hold_key:
+                return False
+            self._toggle_key = key
+            return True
+
+    def toggle(self):
+        with self._lock:
+            self._running = not self._running
+            if self._running:
+                self._controller.press(self._hold_key)
+            else:
+                self._controller.release(self._hold_key)
+        self.running_changed.emit(self._running)
+
+    def stop(self):
+        with self._lock:
+            if self._running:
+                self._controller.release(self._hold_key)
+                self._running = False
+            self._capturing = False
+        if self._listener.running:
+            self._listener.stop()
+
+    @property
+    def is_running(self):
+        with self._lock:
+            return self._running
+
+    @property
+    def hold_key(self):
+        with self._lock:
+            return self._hold_key
+
+    @property
+    def toggle_key(self):
+        with self._lock:
+            return self._toggle_key
